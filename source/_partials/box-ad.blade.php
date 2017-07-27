d60294ff08869706c1d70e2990c704a42a65b1ba
Jan Cyril Segubience <jancyril@segubience.com>
2017-07-27 08:24:38 +0800

    Update box ad

--- source/_partials/box-ad.blade.php
+++ source/_partials/box-ad.blade.php
@@ -1,12 +1,12 @@
 @section('box-ad')
-    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-    <ins class="adsbygoogle"
-         style="display:block; text-align:center;"
-         data-ad-format="fluid"
-         data-ad-layout="in-article"
-         data-ad-client="ca-pub-6767432697631352"
-         data-ad-slot="4435862681"></ins>
-    <script>
-         (adsbygoogle = window.adsbygoogle || []).push({});
-    </script>
+  <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+  <ins class="adsbygoogle"
+       style="display:block; text-align:center;"
+       data-ad-format="fluid"
+       data-ad-layout="in-article"
+       data-ad-client="ca-pub-6767432697631352"
+       data-ad-slot="8563821664"></ins>
+  <script>
+   (adsbygoogle = window.adsbygoogle || []).push({});
+  </script>
 @endsection
